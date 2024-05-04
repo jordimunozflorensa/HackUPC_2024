3de65f8ac426b9d4546a42395974de59fe12848a
jordimunozflorensa <jordi.munoz.florensa@estudiantat.upc.edu>
2024-05-05 01:33:08 +0200

    Afegida funcio per escollir algorisme

---
+++ algorithm.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from tsp import obtener_nombres_camino as tonc
+from sa import obtener_nombres_camino as sonc
+
+def decidir_alg(nombre_csv):
+    df = pd.read_csv(nombre_csv)
+    if len(df) < 16:
+        return tonc(nombre_csv)
+    else:
+        return sonc(nombre_csv)
+    
+print(decidir_alg('lista_productos.csv'))   
